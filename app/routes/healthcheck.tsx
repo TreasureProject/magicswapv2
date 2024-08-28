fe561da0dd64ac0d7c496b7a34a26cd0a9808cef
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-08-28 08:19:58 -0700

    add more logs to healthcheck

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -1,11 +1,13 @@
 import { fetchTokens } from "~/api/tokens.server";
+import { getMeshOptions } from ".graphclient";
 
 export const loader = async () => {
   try {
     await fetchTokens();
     return new Response("OK");
   } catch (err) {
-    console.error("Healthcheck failed:", err);
+    const options = await getMeshOptions();
+    console.error("Healthcheck failed:", err, options);
     return new Response("ERROR", { status: 500 });
   }
 };
