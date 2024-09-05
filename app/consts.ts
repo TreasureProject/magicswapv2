1a6a2652838721c286aed9c4bbc94c40be4609d1
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-09-05 09:40:16 -0700

    fix buy magic url

--- app/consts.ts
+++ app/consts.ts
@@ -4,7 +4,7 @@ export const TERMS_OF_SERVICE_URL = "https://app.treasure.lol/terms-of-service";
 export const DOCS_URL = "https://docs.treasure.lol/magicswap";
 export const GOVERNANCE_FORUM_URL =
   "https://forum.treasure.lol/discussions/3.1.%20Magicswap%20-%20Active";
-export const BUY_MAGIC_URL = "https://app.treasure.lol/manage-funds";
+export const BUY_MAGIC_URL = "https://market.treasure.lol/manage-funds";
 
 export const DISCORD_URL = "https://discord.com/invite/treasuredao";
 export const TWITTER_URL = "https://twitter.com/MagicSwap_";
