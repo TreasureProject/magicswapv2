402d085fac099b4829322fb37d237e585782d319
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2025-01-14 17:45:32 -0800

    upgrade dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,18 @@ installGlobals({
 });
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_lazyRouteDiscovery: true,
+        v3_relativeSplatPath: true,
+        v3_singleFetch: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
   ssr: {
     noExternal: ["react-use"],
   },
