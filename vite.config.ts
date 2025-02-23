3ba27966d45fa89be3d3a9bad26419a3451659f6
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2025-02-22 22:07:04 -0800

    add cloudflare pages configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
+import adapter from "@hono/vite-dev-server/cloudflare";
 import { reactRouter } from "@react-router/dev/vite";
+import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
 import autoprefixer from "autoprefixer";
 import serverAdapter from "hono-react-router-adapter/vite";
 import tailwindcss from "tailwindcss";
@@ -11,9 +13,14 @@ export default defineConfig({
       plugins: [tailwindcss, autoprefixer],
     },
   },
+  build: {
+    assetsInlineLimit: 1024,
+  },
   plugins: [
+    cloudflareDevProxy(),
     reactRouter(),
     serverAdapter({
+      adapter,
       entry: "server/index.ts",
     }),
     tsconfigPaths(),
